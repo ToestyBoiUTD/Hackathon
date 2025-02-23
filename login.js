ce556f2391f7732037b699cd44fd220e60ef6f1b
Michael Baxter <mjb210008@utdallas.edu>
2025-02-23 00:04:39 -0600

    hi

--- login.js
+++ login.js
@@ -1,15 +1,25 @@
 function buttonClick(event){
     event.preventDefault();  // Prevent the form from submitting and refreshing the page
-
-    // Log to confirm the button click and form submission
-    console.log('Sign In button clicked!');
-
-    // Your custom logic here
-    // For example, redirecting to another page or displaying a message
-    redirectToSchedule();
+    const email = document.getElementById('Email').value;
+    const password = document.getElementById('Password').value;
+    const emails = JSON.parse(localStorage.getItem("emails")) || [];
+    const passwords = JSON.parse(localStorage.getItem("passwords")) || [];
+    for (i in emails) {
+        if (emails[i] === email)
+        {
+            if (passwords[i] === password){
+                alert("Successful login! Redirecting to schedule...")
+                window.location.href = "schedule.html"; // Redirects to the schedule page
+                break;
+            }
+            else {
+                alert ("Password incorrect, please try again")
+                return; 
+            }
+            
+        }
+    }
+    alert("No account with that email, please sign up.")
+    return;
 };
-
-function redirectToSchedule(event) {
-    //window.location.href = "schedule.html"; // Redirects to the schedule page
-}
 
--- signup.js
+++ signup.js
@@ -1,5 +1,25 @@
-function redirectToSignup(event) {
-    event.preventDefault(); // Prevents the form from submitting the traditional way
-    window.location.href = "signup.html";
+function signUpButton(event){
+    event.preventDefault(); // Prevent form submission
+    // Get the input values
+    const email = document.getElementById('Email').value;
+    const password = document.getElementById('Password').value;
+    const emails = JSON.parse(localStorage.getItem("emails")) || [];
+    const passwords = JSON.parse(localStorage.getItem("passwords")) || [];
+    for (i in emails) {
+        if (emails[i] === email)
+        {
+            alert("Email already registered! Please log in.");
+            window.location.href = "login.html"; // Redirect to login page
+            return; 
+        }
+    }
+    
+    emails.push((email));
+    passwords.push((password));
+    localStorage.setItem("emails", JSON.stringify(emails));
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+    
+    alert("Sign-up succesful! Redirecting to login...");
+    window.location.href = "login.html"; // Redirect to login page
+    
 }
-   
