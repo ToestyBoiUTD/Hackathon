69ab0d406526596734fe907cc30bc5a4507bd3cb
Michael Baxter <mjb210008@utdallas.edu>
2025-02-23 10:09:09 -0600

    fixing time

--- schedule.js
+++ schedule.js
@@ -1,28 +1,25 @@
 function updateTime() {
     const now = new Date();
-    console.log(now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }))
-    if (now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) === "08:14 AM") {
-        console.log("changed");
-        document.getElementById("pillsButton").disabled = false;
-        document.getElementById("pillsButton").classList.remove("bg-gray-400", "cursor-not-allowed");
-        document.getElementById("pillsButton").classList.add('bg-purple-500');
-
-    }
     document.getElementById("current-time").innerText = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     document.getElementById("current-date").innerText = now.toLocaleDateString(undefined, { month: "long", day: "numeric", year: "numeric" });
-}
+
+    }
 function updateCoins() {
     let coinElement = document.getElementById("current-coins");
     let currentCoins = localStorage.getItem("coins");
+    if (currentCoins === null)
+    {
+        localStorage.setItem("coins", 100)
+        currentCoins = localStorage.getItem("coins");
+    }
     coinElement.innerText = currentCoins;
 
 }
-
-setInterval(updateTime, 60000);
-window.onload = updateTime;
+setInterval(updateTime, 1);
 window.onload = updateCoins;
 
 function completeTask(button) {
+    updateTime;
     button.disabled = true;
     button.classList.add("bg-gray-400", "cursor-not-allowed");
     button.classList.remove('bg-purple-500');
